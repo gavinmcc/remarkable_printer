a37179e71cab9f32a8e11d378e7c06effc3734eb
Gavin McCullagh <gmccullagh@gmail.com>
2021-03-27 06:42:00 +0000

    fix url

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir -p /home/root/bin
 cd /home/root/bin
-wget -O release.zip http://github.com/gavinmcc/remarkable_printer/releases/latest/download/release.zip
+wget -O release.zip http://github.com/gavinmcc/remarkable_printer/releases/7/download/release.zip
 unzip -o release.zip
 mv printer.service /etc/systemd/system
 systemctl daemon-reload
